8f24e87fe222390f456efe22fff9a65b8f686578
hyunji-ok <61300084+hyunji-ok@users.noreply.github.com>
2024-09-29 08:22:49 +0900

    feat: TodoItem컴포넌트 생성 및 insert toggle 기능 추가

--- App.tsx
+++ App.tsx
@@ -13,7 +13,25 @@ function App() {
     {id: 1, text: '작업환경 설정', done: true},
     {id: 2, text: '리액트 네이티브 기초 공부', done: false},
     {id: 3, text: '투두리스트 만들어보기', done: false},
-  ])
+  ]);
+
+  const onInsert = text => {
+    const nextId = todos.length > 0 ? Math.max(...todos.map(todo => todo.id)) + 1 : 1;
+    const todo = {
+      id: nextId,
+      text,
+      done: false,
+    };
+
+    setTodos(todos.concat(todo));
+  };
+
+  const onToggle = id => {
+    const nextTodos = todos.map(todo => 
+      todo.id === id ? {...todo, done: !todo.done} : todo,
+    );
+    setTodos(nextTodos);
+  };
 
   return (
     <SafeAreaProvider>
@@ -23,8 +41,12 @@ function App() {
           style={styles.avoid}
         >
           <DateHead date={today} />
-          {todos.length === 0 ? <Empty /> : <TodoList todos={todos} />}
-          <AddTodo />
+          {todos.length === 0 ? (
+            <Empty />
+            ) : (
+            <TodoList todos={todos} onToggle={onToggle} />
+          )}
+          <AddTodo onInsert={onInsert}  />
         </KeyboardAvoidingView>
       </SafeAreaView>
     </SafeAreaProvider>
--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, View} from 'react-native';
+import TodoItem from './TodoItem';
 
-function TodoList({todos}) {
+function TodoList({todos, onToggle}) {
     return (
         <FlatList
+            ItemSeparatorComponent={() => <View style={styles.separator} />}
             style={styles.list}
             data={todos}
             renderItem={({item}) => (
-                <View>
-                    <Text>{item.text}</Text>
-                </View>
+                <TodoItem
+                    id={item.id}
+                    text={item.text}
+                    done={item.done}
+                    onToggle={onToggle}
+                />
             )}
             keyExtractor={item => item.id.toString()}
         />
@@ -19,6 +24,10 @@ function TodoList({todos}) {
 const styles = StyleSheet.create({
     list: {
         flex: 1,
+    },
+    separator: {
+        backgroundColor: '#e0e0e0',
+        height: 1,
     },
 });
 
